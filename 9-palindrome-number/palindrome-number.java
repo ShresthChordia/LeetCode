b141920970ed604c09243781cb8f7ecd38cd82ce
Shresth Chordia <182196935+ShresthChordia@users.noreply.github.com>
2026-07-27 16:49:41 +0530

    Time: 8 ms (7.65%) | Memory: 45.9 MB (72.66%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,19 +1,6 @@
-class Solution {
+public class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0){
-            return false;
-        }
-        int b=x;
-        int c=0;
-        while(b!=0){
-            c=c*10 + b%10;
-            b/=10;
-        }
-        if(x==c){
-            return true;
-        }
-        else{
-            return false;
-        }
+        String s = String.valueOf(x);
+        return s.equals(new StringBuilder(s).reverse().toString());
     }
 }
