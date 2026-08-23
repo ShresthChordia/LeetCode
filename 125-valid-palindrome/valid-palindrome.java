b9640da253b86f5b36320b254f8b3b0e8b8cfe99
Shresth Chordia <182196935+ShresthChordia@users.noreply.github.com>
2026-08-23 23:39:27 +0530

    Time: 1 ms (100.00%) | Memory: 44.8 MB (41.13%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,12 +1,27 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String x=s.toLowerCase().replaceAll("[^a-zA-Z0-9]","");
-        String y=new StringBuilder(x).reverse().toString();
-        if(y.equals(x)){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+while(left<right && !((s.charAt(left)>='a'&& s.charAt(left)<='z' || s.charAt(left)>='A'&& s.charAt(left)<='Z' || s.charAt(left)>='0' && s.charAt(left)<='9' ))){
+    left++;
 }
+while(left<right && !((s.charAt(right)>='a'&& s.charAt(right)<='z' || s.charAt(right)>='A'&&s.charAt(right)<='Z' || s.charAt(right)>='0' && s.charAt(right)<='9' ))){
+    right--;
+}
+char l=s.charAt(left);
+char r=s.charAt(right);
+if(l>='A'&&l<='Z'){
+    l+=32;
+}
+if(r>='A'&&r<='Z'){
+    r+=32;
+}
+if(l!=r){
+    return false;
+}
+left++;
+right--;
+}
+return true;
+}}
